06990f8fd7adc0694d9b813cb23ead4180d5ba7a
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-05 15:57:06 -0500

    test3

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+
+
 app.use(express.json());
-app.use(express.static('public'));
 
 
-require('./routes/routes')(app);
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
