962427889bbb1288115e4c28265e5ac2643e3299
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-05 15:38:06 -0500

    test

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-
-
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
+require('./routes/routes')(app);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
