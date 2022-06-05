4f711cf605a6abd08e764a096e074194ceebb345
Kyle Reynolds <kbreyn06@gmail.com>
2022-06-05 15:27:43 -0500

    routes

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = requre('path');
+
+module.exports = app => {
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+    
+        app.get('/api/notes', function(req, res) {
+            res.json(notes);
+        });
+    });
+}
